c11da144c2c1eeddc519b67a32fcf42527f58c4b
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-11 14:30:55 +0200

    Putting in SCRIPT_PATH explicitly

--- build.cmd
+++ build.cmd
@@ -1,16 +1,9 @@
 @echo off
-
-echo BUILDING
-
-rem Borrowed and inspired from Akka.net : https://github.com/akkadotnet/akka.net
-
-pushd %~dp0
-
-
 SETLOCAL
+SET SCRIPT_PATH=%~dp0
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-SET NUGET_DIR=.nuget
-SET PACKAGE_DIR=packages
+SET NUGET_DIR=%SCRIPT_PATH%\.nuget
+SET PACKAGE_DIR=%SCRIPT_PATH%\packages
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
@@ -25,17 +18,8 @@ copy %CACHED_NUGET% %NUGET_DIR% > nul
 :restore
 
 %NUGET_DIR%\NuGet.exe update -self
-
-pushd %~dp0
-
-%NUGET_DIR%\NuGet.exe update -self
-
 %NUGET_DIR%\NuGet.exe install FAKE -OutputDirectory %PACKAGE_DIR% -ExcludeVersion -Version 4.16.1
 %NUGET_DIR%\NuGet.exe install FSharp.Data -OutputDirectory %PACKAGE_DIR%\FAKE -ExcludeVersion -Version 2.3.2
 
-rem cls
-
-set encoding=utf-8
-%PACKAGE_DIR%\FAKE\tools\FAKE.exe Fake\build.fsx %*
-
-popd
+SET encoding=utf-8
+%PACKAGE_DIR%\FAKE\tools\FAKE.exe Build\Fake\build.fsx %*
