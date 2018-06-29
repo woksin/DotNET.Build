fc77109fa725ce174b7a142e688668f555c5f265
Sindre Wilting <sindrewilting@gmail.com>
2018-06-29 11:57:07 +0200

    Not knowing any F# or AppVeyor, maybe this will work?

--- Fake/AppVeyor.fsx
+++ Fake/AppVeyor.fsx
@@ -7,7 +7,7 @@ open ProcessHelpers
 
 Target "UpdateVersionOnBuildServer" (fun _ ->
     if( Globals.IsAppVeyor ) then
-        tracef "Updating build version for AppVeyor to %s\n" (Globals.BuildVersion.AsString())
-        let allArgs = sprintf "UpdateBuild -Version \"%s\"" (Globals.BuildVersion.AsString())
+        tracef "Updating build version for AppVeyor to %s-%s\n" (Globals.BuildVersion.AsString(), Globals.BuildNumber.AsString())
+        let allArgs = sprintf "UpdateBuild -Version \"%s-%s\"" (Globals.BuildVersion.AsString(), Globals.BuildNumber.AsString())
         ProcessHelpers.Spawn("appveyor", allArgs) |> ignore
 )
