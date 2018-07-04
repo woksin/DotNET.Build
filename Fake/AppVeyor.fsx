ee5641021294230fc63d1cc1b685b1852648be2b
Einar Ingebrigtsen <einar@dolittle.com>
2018-07-04 12:50:26 +0200

    Should fix #2 - so we don't override build numbers on PRs

--- Fake/AppVeyor.fsx
+++ Fake/AppVeyor.fsx
@@ -2,11 +2,12 @@
 #load "ProcessHelpers.fsx"
 
 open Fake
+open System
 open Globals
 open ProcessHelpers
 
 Target "UpdateVersionOnBuildServer" (fun _ ->
-    if( Globals.IsAppVeyor ) then
+    if( Globals.IsAppVeyor && Globals.IsPullRequest == false ) then
         tracef "Updating build version for AppVeyor to %s\n" (Globals.BuildVersion.AsString())
         let allArgs = sprintf "UpdateBuild -Version \"%s\"" (Globals.BuildVersion.AsString())
         ProcessHelpers.Spawn("appveyor", allArgs) |> ignore
