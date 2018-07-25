d3d364ef76eff5305a288d0254c8dcfe6eb39e30
Sindre Wilting <sindrewilting@gmail.com>
2018-07-25 16:32:01 +0200

    Final touch

--- DeployPackagesLocally.sh
+++ DeployPackagesLocally.sh
@@ -15,15 +15,15 @@ dotnet pack -p:PackageVersion=$PACKAGEVERSION --include-symbols --include-source
 for f in $PACKAGEDIR/*.symbols.nupkg; do
   mv ${f} ${f/.symbols/}
 done
-# Convert files to lowercase
-for i in $PACKAGEDIR/*; do mv $i `echo $i | tr [:upper:] [:lower:]`; done
 
 for f in $PACKAGEDIR/*.nupkg; do
     echo ""
     packagename=$(basename ${f%.2.0.0-alpha2.1000.nupkg})
     target=$TARGETROOT/$packagename/$PACKAGEVERSION
-
+    
     mkdir -pv $target && cp -v $f $target
+    # Unzip package
     tar -xzf $target/$(basename $f) -C $target
-
+    # Create an empty .sha512 file, or else it won't recognize the package for some reason
+    touch $target/$(basename $f).sha512
 done
